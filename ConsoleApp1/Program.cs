ca7c7b59d0e60493642ae5f6ad83c2cb90cb36a8
Illia <axe.kirito@gmail.com>
2023-06-01 13:48:11 +0300

    added Fibanacci series code in Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int numb1 = 0;
+            int numb2 = 1;
+            Console.WriteLine($"1: {numb1}");
+            Console.WriteLine($"2: {numb2}");
+
+
+            for (int i = 3; i < 41; i++)
+            {
+                numb2 += numb1;
+                numb1 = numb2 - numb1;
+                Console.WriteLine($"{i}: {numb2}");
+            }
         }
     }
 }
