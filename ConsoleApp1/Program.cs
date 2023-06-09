9066654466d1b1d8f61bde4dd7d676058fedbee5
Tkachenko Illya <axe.kirito@gmail.com>
2023-06-09 10:24:09 +0300

    Recursive file search

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,81 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var subKeyNames = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
-
-            string[] valueNames= subKeyNames.GetValueNames();
-            foreach (var item in valueNames)
+            string[] drivers = GetRootFolders();
+            Console.WriteLine("Назва файлу або папки: ");
+            string targetName = Console.ReadLine();
+            foreach (string driver in drivers)
             {
-                Console.WriteLine(item);
+                Console.WriteLine("Папка: " + driver);
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("**********************************");
+
+                Action<string> searchAction = new Action<string>(folder => 
+                {
+                    Search(folder, targetName);
+                });
+
+                FolderTraversal(driver, searchAction);
+
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine();
+            }
+        }
+
+        static string[] GetRootFolders()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            string[] rootFolders = new string[drives.Length];
+
+            for (int i = 0; i < drives.Length; i++)
+            {
+                rootFolders[i] = drives[i].RootDirectory.FullName;
+            }
+
+            return rootFolders;
+        }
+
+        static void FolderTraversal(string folder, Action<string> someAction)
+        {
+            try
+            {
+
+
+                foreach (string file in Directory.GetFiles(folder))
+                {
+                    someAction(file);
+                }
+
+                string[] subdirectories;
+                try
+                {
+                    subdirectories = Directory.GetDirectories(folder);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    return;
+                }
+
+                foreach (string subdirectory in subdirectories)
+                {
+                    someAction(subdirectory);
+                    FolderTraversal(subdirectory, someAction);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        static void Search(string path, string targetName)
+        {
+            if (Path.GetFileName(path).Contains(targetName))
+            {
+                
+                Console.WriteLine(path);
+                Console.WriteLine("**********************************");
             }
         }
     }
